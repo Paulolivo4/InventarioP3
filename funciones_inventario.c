ac7aa3f16091710747a322e9e6e90b721f77964a
Paulolivo4 <130105771+Paulolivo4@users.noreply.github.com>
2023-06-27 11:20:29 -0500

    funciones_inventario.c

--- funciones_inventario.c
+++ funciones_inventario.c
@@ -5,14 +5,14 @@
 
 
 
-//Función para imprimir los productos del inventario
+
 void mostrar_inventario(int id[10], char *nombres[10], int cantidad[10], float precios[10])
 {
     printf("\n----------------------------- PAPELERIA -----------------------------\n");
     printf(" CODIGO         NOMBRE                   CANTIDAD         PRECIO   \n");
     printf("---------------------------------------------------------------------\n");
 
-    //Obtener la longitud máxima de los nombres para que se imprima correctamente
+    
     int ancho_nombre = 0;
     for (int i = 0; i < 10; i++)
     {
@@ -23,25 +23,25 @@ void mostrar_inventario(int id[10], char *nombres[10], int cantidad[10], float p
         }
     }
 
-    //Impresión de todos los productos del inventario de forma que se vea todo alineado
+    
     for (int i = 0; i < 10; i++)
     {
-        //Impresión en caso de que el id y la cantidad sean números menores a 10
+        
         if (id[i] < 10 && cantidad[i] < 10)
         {
             printf("   0%d           %-*s                0%d             $%.2f\n", id[i], ancho_nombre, nombres[i], cantidad[i], precios[i]);
         }
-        //Impresión en caso de que el id sean números menores a 10
+        
         else if (id[i] < 10)
         {
             printf("   0%d           %-*s                %d             $%.2f\n", id[i], ancho_nombre, nombres[i], cantidad[i], precios[i]);
         }
-        //Impresión en caso de que la cantidad sean números menores a 10
+        
         else if (cantidad[i] < 10)
         {
             printf("   %d           %-*s                0%d             $%.2f\n", id[i], ancho_nombre, nombres[i], cantidad[i], precios[i]);
         }
-        //Impresión si el id y la cantidad son mayores a 10
+        
         else
         {
             printf("   %d           %-*s                %d             $%.2f\n", id[i], ancho_nombre, nombres[i], cantidad[i], precios[i]);
@@ -50,10 +50,10 @@ void mostrar_inventario(int id[10], char *nombres[10], int cantidad[10], float p
     printf("---------------------------------------------------------------------\n");
 }
 
-//Función para ingresar nuevos productos (dividido en otras funciones)
+
 void ingresar_producto(int id[10], char *nombres[10], char nuevo_nombre[50], int cantidad[10], float precios[10])
 {
-    //Variable contador para saber si el inventario se encuentra lleno
+    
     int contador = 0;
     for (int i = 0; i < 10; i++)
     {
@@ -63,7 +63,7 @@ void ingresar_producto(int id[10], char *nombres[10], char nuevo_nombre[50], int
         }    
     }
 
-    //En caso de que el inventario no esté lleno, se pueden agregar más productos
+    
     if (contador < 10)
     {
         ingresar_producto_id(id);
@@ -72,14 +72,14 @@ void ingresar_producto(int id[10], char *nombres[10], char nuevo_nombre[50], int
         ingresar_producto_precio(precios);
         printf("\n");
     }
-    //Si el inventario está lleno, no se pueden agregar más productos
+    
     else
     {
         printf("El inventario esta lleno.\n\n");
     }
 }
 
-//Función para ingresar ID del nuevo producto
+
 int ingresar_producto_id(int id[10])
 {
     int id_nuevo;
@@ -91,7 +91,7 @@ int ingresar_producto_id(int id[10])
 
         for (int i = 0; i < 10; i++)
         {
-            //Si el ID es menor o igual a cero, o es igual al ID de otro producto, su valor será 0
+            
             if (id_nuevo == id[i] || id_nuevo <= 0)
             {
                 id_nuevo = 0;
@@ -105,7 +105,7 @@ int ingresar_producto_id(int id[10])
             }
         }
 
-        //Si el valor del ID es 0, se tendrá que insertar uno diferente
+        
         if (id_nuevo == 0)
         {
             printf("Ese id ya corresponde a otro producto o es 0. Inserta uno nuevo.\n");
@@ -113,7 +113,7 @@ int ingresar_producto_id(int id[10])
     } while (id_nuevo == 0);
 }
 
-//Minu función para ingresar nombre del nuevo producto
+
 char* ingresar_producto_nombre(char *nombres[10], char nuevo_nombre[50])
 {
     do 
@@ -123,22 +123,22 @@ char* ingresar_producto_nombre(char *nombres[10], char nuevo_nombre[50])
 
         for (int i = 0; i < 10; i++)
         {
-            //Primero se valida si el nombre que se ingresa es igual al de otro producto
+            
             if (strcmp(nuevo_nombre, nombres[i]) == 0)
             {
                 strcpy(nuevo_nombre, "------");
                 break;
             }
-            //En caso de que no lo sea, se retorna el nuevo nombre
+            
             else if (strcmp(nombres[i], "------") == 0)
             {
-                /*Se usa la función strdup para duplicar una cadena en memoria dinámica con eso se
-                evita el uso de malloc()*/ 
+                
+                
                 return nombres[i] = strdup(nuevo_nombre);
             }
         }
 
-        //En caso de que el producto sea igual al de otro, se tiene que ingresar uno nuevo
+        
         if (strcmp(nuevo_nombre, "------") == 0)
         {
             printf("Ese nombre ya corresponde a otro producto. Inserta uno nuevo.\n\n");
@@ -147,7 +147,7 @@ char* ingresar_producto_nombre(char *nombres[10], char nuevo_nombre[50])
 }
 
 
-//Minu función para ingresar la cantidad del nuevo producto
+
 int ingresar_producto_cantidad(int cantidad[10])
 {
     int cantidad_nueva;
@@ -157,7 +157,7 @@ int ingresar_producto_cantidad(int cantidad[10])
         printf("Inserta la cantidad del producto: ");
         scanf("%d", &cantidad_nueva);
 
-        //Se valida que la cantidad sea mayor a 0
+        
         if (cantidad_nueva <= 0)
         {
             printf("La cantidad debe ser mayor a 0. Inserta otra vez.\n\n");
@@ -175,7 +175,7 @@ int ingresar_producto_cantidad(int cantidad[10])
 }
 
 
-//Minu función para ingresar el precio del nuevo producto
+
 float ingresar_producto_precio(float precios[10])
 {
     float precio_nuevo;
@@ -185,7 +185,7 @@ float ingresar_producto_precio(float precios[10])
         printf("Inserta el precio del producto: ");
         scanf("%f", &precio_nuevo);
 
-        //Se valida que el precio del producto sea mayor a $0.00
+       
         if (precio_nuevo <= 0)
         {
             printf("El precio debe ser mayor a 0. Inserta otra vez.\n\n");
@@ -204,7 +204,7 @@ float ingresar_producto_precio(float precios[10])
 }
 
 
-//Función para modificar la cantidad de un producto
+
 int modificar_cantidad(int id[10], int cantidad[10])
 {
     int id_producto;
@@ -215,14 +215,14 @@ int modificar_cantidad(int id[10], int cantidad[10])
     printf("\nInserta el ID del producto para modificar su cantidad: ");
     scanf("%d", &id_producto);
 
-    //Primero se valida que el ID del producto a modificar sea válido
+    
     if (id_producto == 0)
     {
         printf("El ID 0 no es valido.\n\n");
         return 1;
     }
 
-    //Luego que el ID sea de un producto que esté en el inventario
+    
     for (int i = 0; i < 10; i++)
     {
         if (id_producto == id[i])
@@ -232,31 +232,31 @@ int modificar_cantidad(int id[10], int cantidad[10])
         }
         else if (i == 9)
         {
-            //En caso de que el ID no sea de un producto del inventario, este valdrá 0
+            
             id_producto = 0;
         }
     }
 
-    //Si el ID es 0, no es el ID de un producto del inventario
+    
     if (id_producto == 0)
     {
         printf("No es el ID de un producto.\n\n");
         return 1;
     }
 
-    //Insertar cantidad modificada del producto
+   
     do
     {
         printf("Inserta la nueva cantidad del producto: ");
         scanf("%d", &nueva_cantidad);        
-        //La cantidad no puede ser igual a la anterior
+        
         if (nueva_cantidad == cantidad[posicion])
         {
             printf("Esa es la cantidad actual del producto. Inserta una nueva.\n\n");
         }
         else
         {
-            //La cantidad no puede ser menor o igual a 0
+            
             if (nueva_cantidad <= 0)
             {
                 printf("La cantidad no puede ser 0 o menor. Inserta una nueva\n\n");
@@ -271,7 +271,7 @@ int modificar_cantidad(int id[10], int cantidad[10])
 }
 
 
-//Función para eliminar un producto del inventario
+
 int eliminar_producto(int id[10], char *nombres[10], char nuevo_nombre[50], int cantidad[10], float precios[10])
 {
     int id_producto;
@@ -281,14 +281,14 @@ int eliminar_producto(int id[10], char *nombres[10], char nuevo_nombre[50], int 
     printf("\nInserta el ID del producto para eliminarlo: ");
     scanf("%d", &id_producto);
 
-    //Se valida que el ID del producto no sea 0
+    
     if (id_producto == 0)
     {
         printf("El ID 0 no es valido.\n\n");
         return 1;
     }
 
-    //Se valida que el ID del producto sea de uno que se encuentre en el inventario
+    
     for (int i = 0; i < 10; i++)
     {
         if (id_producto == id[i])
@@ -302,14 +302,14 @@ int eliminar_producto(int id[10], char *nombres[10], char nuevo_nombre[50], int 
         }
     }
 
-    //En caso de que el ID sea 0, signfica que no es el ID de un producto del inventario
+    
     if (id_producto == 0)
     {
         printf("No es el ID de un producto.\n\n");
         return 1;
     }
 
-    //Se restauran todos los valores de ese producto y se elimina
+    
     id[posicion] = 0;
     nombres[posicion] = "------";
     cantidad[posicion] = 0;
